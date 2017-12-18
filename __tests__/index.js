6184ea89b2ced0ed39fc9b1a36864b89c5f14b13
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-12-18 23:28:51 +0100

    fix methods with no args

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,85 +2,99 @@ const fs = require("fs");
 const javaMethodParser = require("../");
 
 function loadFile(name) {
-  return fs.readFileSync(`./exampleFiles/${name}.java`, "utf8");
+	return fs.readFileSync(`./exampleFiles/${name}.java`, "utf8");
 }
 
 describe("java-method-parser", () => {
-  describe("basic example", () => {
-    const basic = loadFile("basic");
-    it("should return name of basic example", () => {
-      expect(javaMethodParser(basic).name).toBe("BasicName");
-    });
+	describe("basic example", () => {
+		const basic = loadFile("basic");
+		it("should return name of basic example", () => {
+			expect(javaMethodParser(basic).name).toBe("BasicName");
+		});
 
-    it("should return the package name", () => {
-      expect(javaMethodParser(basic).package).toBe("com.wix.detox.espresso");
-    });
+		it("should return the package name", () => {
+			expect(javaMethodParser(basic).package).toBe("com.wix.detox.espresso");
+		});
 
-    it("should return five methods for basic example", () => {
-      expect(javaMethodParser(basic).methods.length).toBe(7);
-    });
+		it("should return five methods for basic example", () => {
+			expect(javaMethodParser(basic).methods.length).toBe(7);
+		});
 
-    it("should detect private and public methods", () => {
-      expect(javaMethodParser(basic).methods.map(x => x.public)).toEqual([
-        true,
-        true,
-        true,
-        true,
-        true,
-        false,
-        true
-      ]);
-    });
+		it("should detect private and public methods", () => {
+			expect(javaMethodParser(basic).methods.map(x => x.public)).toEqual([
+				true,
+				true,
+				true,
+				true,
+				true,
+				false,
+				true
+			]);
+		});
 
-    it("should be able to detect static methods", () => {
-      expect(javaMethodParser(basic).methods.map(x => x.static)).toEqual([
-        true,
-        true,
-        true,
-        true,
-        false,
-        true,
-        false
-      ]);
-    });
+		it("should be able to detect static methods", () => {
+			expect(javaMethodParser(basic).methods.map(x => x.static)).toEqual([
+				true,
+				true,
+				true,
+				true,
+				false,
+				true,
+				false
+			]);
+		});
 
-    it("should parse the arguments", () => {
-      const [firstMethod, secondMethod] = javaMethodParser(basic).methods;
-      expect(firstMethod.args.length).toBe(1);
-      expect(firstMethod.args[0].type).toBe("int");
-      expect(firstMethod.args[0].name).toBe("times");
-      expect(firstMethod.args[0].final).toBe(false);
+		it("should parse the arguments", () => {
+			const [firstMethod, secondMethod] = javaMethodParser(basic).methods;
+			expect(firstMethod.args.length).toBe(1);
+			expect(firstMethod.args[0].type).toBe("int");
+			expect(firstMethod.args[0].name).toBe("times");
+			expect(firstMethod.args[0].final).toBe(false);
 
-      expect(secondMethod.args.length).toBe(2);
-      expect(secondMethod.args[0].type).toBe("String");
-      expect(secondMethod.args[0].name).toBe("x");
-      expect(secondMethod.args[0].final).toBe(true);
+			expect(secondMethod.args.length).toBe(2);
+			expect(secondMethod.args[0].type).toBe("String");
+			expect(secondMethod.args[0].name).toBe("x");
+			expect(secondMethod.args[0].final).toBe(true);
 
-      expect(secondMethod.args[1].type).toBe("float");
-      expect(secondMethod.args[1].name).toBe("y");
-      expect(secondMethod.args[1].final).toBe(true);
-    });
-  });
+			expect(secondMethod.args[1].type).toBe("float");
+			expect(secondMethod.args[1].name).toBe("y");
+			expect(secondMethod.args[1].final).toBe(true);
+		});
+	});
 
-  describe("advanced example", () => {
-    const advanced = loadFile("advanced");
+	describe("advanced example", () => {
+		const advanced = loadFile("advanced");
 
-    it("should ignore inner anonymous functions", () => {
-      expect(javaMethodParser(advanced).methods.length).toBe(7);
-    });
-  });
+		it("should ignore inner anonymous functions", () => {
+			expect(javaMethodParser(advanced).methods.length).toBe(7);
+		});
+	});
 
-  describe("advanced2 example", () => {
-    const advanced = loadFile("advanced2");
+	describe("advanced2 example", () => {
+		const advanced = loadFile("advanced2");
 
-    it("should allow comment in the beginning", () => {
-      expect(javaMethodParser(advanced).package).toBe(
-        "android.support.test.espresso.action"
-      );
-    });
+		it("should allow comment in the beginning", () => {
+			expect(javaMethodParser(advanced).package).toBe(
+				"android.support.test.espresso.action"
+			);
+		});
 
-    it("should allow modifiers for class names", () => {
-      expect(javaMethodParser(advanced).name).toBe("ViewActions");
-    });
-  });
+		it("should allow modifiers for class names", () => {
+			expect(javaMethodParser(advanced).name).toBe("ViewActions");
+		});
+
+		it("should allow modifiers for method args", () => {
+			expect(javaMethodParser(advanced).methods.length).toBe(24);
+			expect(javaMethodParser(advanced).methods[3].args[0].type).toBe(
+				"ViewAction"
+			);
+			expect(javaMethodParser(advanced).methods[3].args[0].name).toBe(
+				"viewAction"
+			);
+		});
+
+		it("should allow methods without args", () => {
+			expect(javaMethodParser(advanced).methods[2].args.length).toBe(0);
+		});
+	});
 });
--- index.js
+++ index.js
@@ -1,118 +1,121 @@
 "use strict";
 
 const parsePackageName = file => {
-  const packageNameRegex = /package (.*)\;/;
-  const [, pkg] = packageNameRegex.exec(file);
-  return pkg;
+	const packageNameRegex = /package (.*)\;/;
+	const [, pkg] = packageNameRegex.exec(file);
+	return pkg;
 };
 
 const parseClassName = file => {
-  // TODO: add extends as edge case
-  const classNameRegex = /public (?:final |static | )*class (.*)\s\{/;
-  const [, name] = classNameRegex.exec(file);
-  return name;
+	// TODO: add extends as edge case
+	const classNameRegex = /public (?:final |static | )*class (.*)\s\{/;
+	const [, name] = classNameRegex.exec(file);
+	return name;
 };
 
 const argumentRegex = /(final)? (\w*) (\w*)/;
 const transformArgument = rawArguments => {
-  const argumentParts = rawArguments
-    .split(" ")
-    .filter(part => part && part !== "\n");
-  if (!argumentParts.length) {
-    return null;
-  }
+	const argumentParts = rawArguments
+		.split(" ")
+		.filter(part => part && part !== "\n");
+	if (!argumentParts.length) {
+		return null;
+	}
 
-  // TODO: check for other modifiers like final
-  if (argumentParts[0] === "final") {
-    const [, type, name] = argumentParts;
-    return {
-      final: true,
-      type,
-      name
-    };
-  }
-  const [type, name] = argumentParts;
-  return {
-    final: false,
-    type,
-    name
-  };
+	// TODO: check for other modifiers like final
+	if (argumentParts[0] === "final") {
+		const [, type, name] = argumentParts;
+		return {
+			final: true,
+			type,
+			name
+		};
+	}
+	const [type, name] = argumentParts;
+	return {
+		final: false,
+		type,
+		name
+	};
 };
 
 // Get the matching closing bracket after this point
 const getClosingBracketPosition = (file, startIndex) => {
-  let currentDepth = 1;
-  let currentIndex = startIndex;
+	let currentDepth = 1;
+	let currentIndex = startIndex;
 
-  while (currentDepth > 0) {
-    const nextOpeningIndex =
-      file.indexOf("{", currentIndex) !== -1
-        ? file.indexOf("{", currentIndex)
-        : Infinity;
-    const nextClosingIndex =
-      file.indexOf("}", currentIndex) !== -1
-        ? file.indexOf("}", currentIndex)
-        : Infinity;
+	while (currentDepth > 0) {
+		const nextOpeningIndex =
+			file.indexOf("{", currentIndex) !== -1
+				? file.indexOf("{", currentIndex)
+				: Infinity;
+		const nextClosingIndex =
+			file.indexOf("}", currentIndex) !== -1
+				? file.indexOf("}", currentIndex)
+				: Infinity;
 
-    // Opening bracket comes next
-    if (nextOpeningIndex < nextClosingIndex) {
-      currentDepth += 1;
-      currentIndex = nextOpeningIndex + 1;
-    }
+		// Opening bracket comes next
+		if (nextOpeningIndex < nextClosingIndex) {
+			currentDepth += 1;
+			currentIndex = nextOpeningIndex + 1;
+		}
 
-    // Closing bracket comes next
-    if (nextClosingIndex < nextOpeningIndex) {
-      currentDepth -= 1;
-      currentIndex = nextClosingIndex + 1;
-    }
-  }
+		// Closing bracket comes next
+		if (nextClosingIndex < nextOpeningIndex) {
+			currentDepth -= 1;
+			currentIndex = nextClosingIndex + 1;
+		}
+	}
 
-  return currentIndex;
+	return currentIndex;
 };
 
 const parseMethods = file => {
-  // TODO: multi line methods
-  const methodRegex = /(public|private|protected) (static )?(\w*) (\w*)(?:\(((\w|\s|\,|\n)*)\))/g;
-  const methods = [];
-  let match;
+	// TODO: multi line methods
+	const methodRegex = /(public|private|protected) (static )?(\w*) (\w*)(?:\(((\w|\s|\,|\n)*)\))/g;
+	const methods = [];
+	let match;
 
-  while ((match = methodRegex.exec(file))) {
-    const start = file.indexOf("{", match.index);
-    const end = getClosingBracketPosition(file, start + 1);
+	while ((match = methodRegex.exec(file))) {
+		const start = file.indexOf("{", match.index);
+		const end = getClosingBracketPosition(file, start + 1);
 
-    const [, visibility, modifier, returnType, name, rawArguments] = match;
+		const [, visibility, modifier, returnType, name, rawArguments] = match;
 
-    methods.push({
-      start,
-      end,
-      content: {
-        public: visibility === "public",
-        static: Boolean(modifier),
-        returnType,
-        name,
-        args: rawArguments.split(",").map(transformArgument)
-      }
-    });
-  }
+		methods.push({
+			start,
+			end,
+			content: {
+				public: visibility === "public",
+				static: Boolean(modifier),
+				returnType,
+				name,
+				args: rawArguments
+					.split(",")
+					.map(transformArgument)
+					.filter(Boolean)
+			}
+		});
+	}
 
-  return methods
-    .filter(
-      (filteredMethod, index, methods) =>
-        !methods.some(
-          comparingMethod =>
-            comparingMethod.start < filteredMethod.start &&
-            filteredMethod.start < comparingMethod.end
-        )
-    )
-    .map(({ content }) => content);
+	return methods
+		.filter(
+			(filteredMethod, index, methods) =>
+				!methods.some(
+					comparingMethod =>
+						comparingMethod.start < filteredMethod.start &&
+						filteredMethod.start < comparingMethod.end
+				)
+		)
+		.map(({ content }) => content);
 };
 
 const parse = file => {
-  return {
-    package: parsePackageName(file),
-    name: parseClassName(file),
-    methods: parseMethods(file)
-  };
+	return {
+		package: parsePackageName(file),
+		name: parseClassName(file),
+		methods: parseMethods(file)
+	};
 };
 
 module.exports = parse;
